3a0026190da56c960a02a5459daca9e43c0b1f61
KRocioCC <krociocalderon@gmail.com>
2026-02-20 13:58:19 -0400

    actualizando para que sea microservicios, con message pattern

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -51,10 +51,11 @@ export class ProductsService {
   }
 
   async update(id: number, updateProductDto: UpdateProductDto) {
+    const { id: __, ...data } = updateProductDto;
     await this.findOne(id);
     return this.prisma.product.update({
       where: { id },
-      data: updateProductDto,
+      data: data,
     });
   }
 

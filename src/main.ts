297477d9e8e153d2d8af13a2c911c6cd4527c821
KRocioCC <krociocalderon@gmail.com>
2026-02-20 13:27:43 -0400

    conviertiendolo en MICROSERVICIO

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,7 +22,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(envs.port);
+  await app.listen();
   logger.log(`Products microservice is running on port ${envs.port}`);
 }
 bootstrap();
